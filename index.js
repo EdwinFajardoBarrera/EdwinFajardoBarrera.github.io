b2b101a40f72bec8508ec0bb19a3e2b9396a5313
Edwin Fajardo <erofaba@gmail.com>
2020-06-10 21:39:52 -0500

    I created a about us info

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 let navBtn = document.querySelector(".burger-btn");
 let menu = document.querySelector(".menu");
+let about = document.querySelector("#call2action");
+let lp = document.querySelector(".left-panel");
+let rp = document.querySelector(".right-panel");
+let us = document.querySelector("#us");
+
+about.addEventListener("click", () => {
+  lp.classList.toggle("toggle-l");
+  rp.classList.toggle("toggle-r");
+  us.classList.toggle("us");
+});
 
 navBtn.addEventListener("click", () => {
   menu.classList.toggle("menu-open");
-})
+});
